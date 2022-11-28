4fbe01cd579afd4bd940b06a9e5b8af26bd12a5b
BenSimmers <bej1380@gmail.com>
2022-11-28 22:18:23 +1000

    initial

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./App.css";
+import { TypeAnimation } from "react-type-animation";
+
+const Title = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        padding: 5,
+      }}
+    >
+      <TypeAnimation
+        sequence={[
+          `Hi, I'm Ben Simmers`,
+          2000, // Waits 2s
+        
+          `I work as full stack web developer`,
+          2000, // Waits 2s
+          //got to a new line
+          `and currently studying Computer Science at QUT`,
+          2000,
+        ]}
+        wrapper="div"
+        cursor={true}
+        repeat={Infinity}
+        style={{ fontSize: "4em", padding: 5 }}
+      />
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Title">
+        <Title />
+      </div>
+    </div>
+  );
+}
+
+export default App;
