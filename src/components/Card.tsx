ed03fe9da5fcfd3edaaf5f60dbd043833142da95
Benjamin Simmers <bej1380@gmail.com>
2022-12-03 13:53:34 +1000

    change

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Card(props: any) {
   return (
     <>
-      <div style={{paddingTop: 20}}/>
+      <div style={{ paddingTop: 20 }} />
       <div className="w-full p-4 text-center bg-gray-800  border rounded-lg shadow-md sm:p-8 dark:bg-gray-800 border-gray-700">
         <h5 className="mb-2 text-3xl font-bold text-white dark:text-white">
           {props.title}
