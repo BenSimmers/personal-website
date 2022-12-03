29c159739e216e6a29dfe729eee27492f4e6548e
Benjamin Simmers <bej1380@gmail.com>
2022-12-03 13:45:02 +1000

    card fixes

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,11 +4,11 @@ export default function Card(props: any) {
   return (
     <>
       <div style={{paddingTop: 20}}/>
-      <div className="w-full p-4 text-center bg-white border rounded-lg shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-        <h5 className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">
+      <div className="w-full p-4 text-center bg-gray-800  border rounded-lg shadow-md sm:p-8 dark:bg-gray-800 border-gray-700">
+        <h5 className="mb-2 text-3xl font-bold text-white dark:text-white">
           {props.title}
         </h5>
-        <p className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">
+        <p className="mb-5 text-base text-gray-400 sm:text-lg dark:text-gray-400">
           {props.description}
         </p>
       </div>
